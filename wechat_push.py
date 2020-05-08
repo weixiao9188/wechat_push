bc96a43990ec73007746028d22a2aadeafadcf08
weixiao <1260531376@qq.com>
2020-05-08 17:51:42 +0800

    利用微信推送通用漏洞3.0 利用死循环实现定时爬虫数据并提交

--- wechat_push.py
+++ wechat_push.py
@@ -1,20 +1,24 @@
 import json
 import requests
-response1 = requests.get(url="https://api.github.com/search/repositories?q=CVE-2020&sort=updated&order=desc")
-response2 = requests.get(url="https://api.github.com/search/repositories?q=RCE&sort=updated&order=desc")
+import time
+while True:
+    response1 = requests.get(url="https://api.github.com/search/repositories?q=CVE-2020&sort=updated&order=desc")
+    response2 = requests.get(url="https://api.github.com/search/repositories?q=RCE&sort=updated&order=desc")
 
-    #print(response1.text)
-    #print(response2.text)
-data1 = json.loads(response1.text)
-data2 = json.loads(response2.text)
-all_datas  = []
-for i in data1['items']:
-    s = [i['name'],i['html_url'],i['description']]
-    if s not in all_datas:
-        params = {
-            "text":s[0],
-            "desp":" 链接:"+str(s[1])+"\n简介"+str(s[2])
-        }
-        requests.get("https://sc.ftqq.com/SCU96954T628ed5500565032d8e20f10e914058925eb4be254fcde.send",params=params)
-        print("推送完成")
-        all_datas.append(s)
+    # print(response1.text)
+    # print(response2.text)
+    data1 = json.loads(response1.text)
+    data2 = json.loads(response2.text)
+    all_datas = []
+    for i in data1['items']:
+        s = [i['name'],i['html_url'],i['description']]
+        if s not in all_datas:
+            params = {
+                "text":s[0],
+                "desp":" 链接:"+str(s[1])+"\n简介"+str(s[2])
+            }
+            requests.get("https://sc.ftqq.com/SCU96954T628ed5500565032d8e20f10e914058925eb4be254fcde.send",params=params)
+            print("推送完成")
+            all_datas.append(s)
+
+    time.sleep(600)
