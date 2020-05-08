13f728155449b33708f2df63b0b70782ba314d15
weixiao <1260531376@qq.com>
2020-05-09 00:06:53 +0800

    利用微信推送CVE-2020  3.0 利用死循环实现定时爬虫数据并提交

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # wechat_push
-利用微信推送通用漏洞1.0
+利用微信推送通用漏洞3.0
+XXX表示通过server酱获取的微信推送接口
+数据保存在当前路径下的olddata.csv（代码运行的时候不要打开此文件，否则会报错）
+当前代码运行需要预先安装numpy pandas requests库，一般anaconda的环境就行
+可以通过更改time_sleep设置爬取间隔时间
--- wechat_push.py
+++ wechat_push.py
@@ -1,24 +1,36 @@
-import json
 import requests
+import json
 import time
-while True:
-    response1 = requests.get(url="https://api.github.com/search/repositories?q=CVE-2020&sort=updated&order=desc")
-    response2 = requests.get(url="https://api.github.com/search/repositories?q=RCE&sort=updated&order=desc")
-
-    # print(response1.text)
-    # print(response2.text)
+import os
+import pandas as pd
+time_sleep = 20 #每隔20秒爬取一次
+while(True):
+    #判断文件是否存在
+    datas = []
+    if os.path.exists("olddata.csv"):
+        datas = pd.read_csv("olddata.csv", header=None).values.tolist()
+    else:
+        datas = []
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.25 Safari/537.36 Core/1.70.3741.400 QQBrowser/10.5.3863.400"}
+    response1 = requests.get(url="https://api.github.com/search/repositories?q=CVE-2020&sort=updated&order=desc",headers=headers)
+    response2 = requests.get(url="https://api.github.com/search/repositories?q=RCE&sort=updated&order=desc",headers=headers)
     data1 = json.loads(response1.text)
     data2 = json.loads(response2.text)
-    all_datas = []
-    for i in data1['items']:
-        s = [i['name'],i['html_url'],i['description']]
-        if s not in all_datas:
-            params = {
-                "text":s[0],
-                "desp":" 链接:"+str(s[1])+"\n简介"+str(s[2])
-            }
-            requests.get("https://sc.ftqq.com/SCU96954T628ed5500565032d8e20f10e914058925eb4be254fcde.send",params=params)
-            print("推送完成")
-            all_datas.append(s)
-
-    time.sleep(600)
+    for j in [data1["items"],data2["items"]]:
+        for i in j:
+            s = {"name":i['name'],"html":i['html_url'],"description":i['description']}
+            s1 = [i['name'],i['html_url'],i['description']]
+            if s1 not in datas:
+                params = {
+                     "text":s["name"],
+                    "desp":" 链接:"+str(s["html"])+"\n简介"+str(s["description"])
+                }
+                print("当前推送为"+str(s))
+                requests.get("https://sc.ftqq.com/SCU96954T628ed5500565032d8e20f10e914058925eb4be254fcde.send",params=params,headers=headers)
+                time.sleep(1)#以防推送太猛
+                print("推送完成")
+                datas.append(s1)
+            else:
+                print("数据已处在")
+    pd.DataFrame(datas).to_csv("olddata.csv",header=None,index=None)
+    time.sleep(time_sleep)
