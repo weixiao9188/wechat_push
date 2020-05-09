214c95152f262a53f944f60704f6c06ebc21742e
weixiao <1260531376@qq.com>
2020-05-09 22:45:03 +0800

    利用微信推送CVE-2020  4.0 处理了上个版本中推送数据重复的问题，ssl问题

--- wechat_push.py
+++ wechat_push.py
@@ -1,7 +1,7 @@
 # Title: wechat push CVE-2020
 # Date: 2020-5-9
 # Exploit Author: weixiao9188
-# Version: 3.0
+# Version: 4.0
 # Tested on: Linux,windows
 # coding:UTF-8
 import requests
@@ -11,16 +11,26 @@ import os
 import pandas as pd
 time_sleep = 20 #每隔20秒爬取一次
 while(True):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.25 Safari/537.36 Core/1.70.3741.400 QQBrowser/10.5.3863.400"}
     #判断文件是否存在
     datas = []
+    response1=None
+    response2=None
     if os.path.exists("olddata.csv"):
+        #如果文件存在则每次爬取10个
         df = pd.read_csv("olddata.csv", header=None)
         datas = df.where(df.notnull(),None).values.tolist()#将提取出来的数据中的nan转化为None
+        response1 = requests.get(url="https://api.github.com/search/repositories?q=CVE-2020&sort=updated&per_page=10",
+                                 headers=headers)
+        response2 = requests.get(url="https://api.github.com/search/repositories?q=RCE&ssort=updated&per_page=10",
+                                 headers=headers)
+
     else:
+        #不存在爬取全部
         datas = []
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.25 Safari/537.36 Core/1.70.3741.400 QQBrowser/10.5.3863.400"}
-    response1 = requests.get(url="https://api.github.com/search/repositories?q=CVE-2020&sort=updated&per_page=10",headers=headers)
-    response2 = requests.get(url="https://api.github.com/search/repositories?q=RCE&ssort=updated&per_page=10",headers=headers)
+        response1 = requests.get(url="https://api.github.com/search/repositories?q=CVE-2020&sort=updated&order=desc",headers=headers)
+        response2 = requests.get(url="https://api.github.com/search/repositories?q=RCE&ssort=updated&order=desc",headers=headers)
 
     data1 = json.loads(response1.text)
     data2 = json.loads(response2.text)
@@ -37,11 +47,12 @@ while(True):
                 }
                 print("当前推送为"+str(s)+"\n")
                 print(params)
-                requests.get("https://sc.ftqq.com/XXXX.send",params=params)
-                time.sleep(1)#以防推送太猛
+                requests.get("https://sc.ftqq.com/XXXX.send",params=params,timeout=10)
+                #time.sleep(1)#以防推送太猛
                 print("推送完成!")
                 datas.append(s1)
             else:
-                print("数据已处在!")
+                pass
+                #print("数据已处在!")
     pd.DataFrame(datas).to_csv("olddata.csv",header=None,index=None)
     time.sleep(time_sleep)
